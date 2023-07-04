ae7d5ca58a864032fe78b5f81d2bb001db52503e
Umair Khan <umairkhan5669@gmail.com>
2023-07-03 20:38:34 -0400

    Added picture

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 50) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#1aff00 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+
 const doc = document;
 const menuOpen = doc.querySelector(".menu");
 const menuClose = doc.querySelector(".close");
@@ -11,3 +31,11 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+window.addEventListener("scroll", function(){
+const header = document.getElementsByClassName('header-19')[0]
+header.classList.toggle("sticky", window.scrollY > 50)
+})
